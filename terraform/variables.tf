d969ae52729d9655f4504e01d1e6a5eb7514d13b
gromozek <sva1973@yandex.ru>
2022-06-14 05:35:53 +0000

    added manifest files

---
+++ terraform/variables.tf
@@ -0,0 +1,55 @@
+#keys
+variable "token" {
+  type      = string
+  sensitive = true
+}
+
+#ID облака
+variable "cloud_id" {
+  type    = string
+  default = "b1g3ub80oi6o4m21td0m"
+}
+
+#ID каталога
+variable "folder_id" {
+  type    = string
+  default = "b1g3o5j1iupdj50va1fn"
+}
+
+#Зона доступности
+variable "zone" {
+  type    = string
+  default = "ru-central1-a"
+}
+
+#Имя подсети, в которой создаются ВМ
+variable "subnet_name" {
+  type    = string
+  default = "subnet"
+}
+
+variable "hosts" {
+    type = map(map(map(string)))
+    default = {
+      prod = {
+	vm1 = {
+          name = "node-01-prod"
+          cores = "4"
+          core_fraction = "100"
+	}
+        vm2 = {
+          name = "node-02-prod"
+          cores = "4"
+          core_fraction = "100"
+        }
+
+    }
+      stage = {
+        vm1 = {
+          name = "node-01-stage"
+          cores = "2"
+          core_fraction = "20"
+        }
+    }
+    }
+}
